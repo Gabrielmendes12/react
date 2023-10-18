14b613444b0d34791ffcbcfc7637803760ee1150
Gabriel Mendes <gabrielmendes0102@gmail.com>
2023-10-18 16:40:12 -0300

    atualização

--- src/pages/Primeiro.jsx
+++ src/pages/Primeiro.jsx
@@ -1,10 +1,32 @@
 import LinkSegundo from "../components/LinkSegundo";
+import LinkGenerico from "../components/LinkGenerico";
 
 const Primeiro = () => {
+
+    const dados = [
+        ["https://www.ibmec.br", "ibmec"],
+
+        ["https://estude.ibmec.br/ibmec-hubs", "ibmec-hubs"],
+
+        ["https://botafogo.com.br/", "botafogo"]
+    ];
+
+    const links = dados.map( (ele) => (
+        <LinkGenerico
+        key={ele[1]}
+        texto={ele[1]}
+        endereco={ele[0]}
+        />
+    ))
+
     return (<div>
-                <h5>meu título</h5>
+                <h3>Meu título</h3>
                 <p>Meu texto</p>
+                {links}
                 <LinkSegundo/>
+                <LinkGenerico 
+                texto = 'texto do link'
+                endereco = 'https://g1.globo.com/'/>
             </div>);
 };
 
